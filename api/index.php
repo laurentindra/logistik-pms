381b888888fd4a880a9aa6cef7823fd143ce8062
Laurent Indra <laurentindra@users.noreply.github.com>
2026-08-11 12:12:29 +0800

    Fix Vercel read-only filesystem log error by routing storage to /tmp and LOG_CHANNEL to stderr

--- api/index.php
+++ api/index.php
@@ -13,7 +13,10 @@ if (!file_exists($dbPath)) {
 
 // Ensure storage directories exist in /tmp
 $storageDirs = [
+    '/tmp/storage',
+    '/tmp/storage/framework',
     '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
     '/tmp/storage/framework/cache/data',
     '/tmp/storage/framework/sessions',
     '/tmp/storage/logs',
@@ -26,14 +29,24 @@ foreach ($storageDirs as $dir) {
 }
 
 // Environment variables for Vercel Serverless
+putenv('VERCEL=1');
+putenv('LOG_CHANNEL=stderr');
 putenv('DB_CONNECTION=sqlite');
 putenv('DB_DATABASE=' . $dbPath);
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 putenv('APP_STORAGE=/tmp/storage');
+
+$_ENV['VERCEL'] = '1';
+$_ENV['LOG_CHANNEL'] = 'stderr';
 $_ENV['DB_CONNECTION'] = 'sqlite';
 $_ENV['DB_DATABASE'] = $dbPath;
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
 $_ENV['APP_STORAGE'] = '/tmp/storage';
+
+$_SERVER['VERCEL'] = '1';
+$_SERVER['LOG_CHANNEL'] = 'stderr';
+$_SERVER['DB_CONNECTION'] = 'sqlite';
+$_SERVER['DB_DATABASE'] = $dbPath;
 
 // Forward to Laravel entrypoint
 require __DIR__ . '/../public/index.php';
